9f4b4a956e00592df2ace81009a592c80bbc2a25
Igor <fullstack@naladka.su>
2023-02-17 21:19:56 +0300

    edit tasks3

--- Program.cs
+++ Program.cs
@@ -47,61 +47,71 @@
 	}
 }
 
+int ReadInput(string message)
+{
+	Console.WriteLine(message);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+
+int[] InputArray(int length)
+{
+	int[] array = new int[length];
+	for (int i = 0; i < length; i++)
+	{
+		array[i] = ReadInput($"Введите {i + 1} число");
+	}
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	Console.WriteLine("Введены следующие числа: ");
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.WriteLine($"=> {array[i]}");
+	}
+}
+
+int CountPositiveNumbers(int[] array)
+{
+	int count = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] > 0)
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
 void Task_1()
 {
-	int ReadInput(string message)
-	{
-		Console.WriteLine(message);
-		string value = Console.ReadLine();
-		int result = Convert.ToInt32(value);
-		return result;
-	}
-
-	int[] InputArray(int length)
-	{
-		int[] array = new int[length];
-		for (int i = 0; i < length; i++)
-		{
-			array[i] = ReadInput($"Введите {i + 1} число");
-		}
-		return array;
-	}
-
-	void PrintArray(int[] array)
-	{
-		Console.WriteLine("Введены следующие числа: ");
-		for (int i = 0; i < array.Length; i++)
-		{
-			Console.WriteLine($"=> {array[i]}");
-		}
-	}
-
-	int CountPosNumbers(int[] array)
-	{
-		int count = 0;
-		for (int i = 0; i < array.Length; i++)
-		{
-			if (array[i] > 0)
-			{
-				count++;
-			}
-		}
-		return count;
-	}
-
 	Console.Clear();
 	int length = ReadInput("Сколько будет чисел? >");
 	int[] array;
 	array = InputArray(length);
 	PrintArray(array);
-	Console.WriteLine($"Чисел больше 0 => {CountPosNumbers(array)}");
+	Console.WriteLine($"Чисел больше 0 => {CountPositiveNumbers(array)}");
 }
 
 void Task_2()
 {
+	double k1 = ReadInput("k1");
+	double b1 = ReadInput("b1");
+	double k2 = ReadInput("k2");
+	double b2 = ReadInput("b2");
 
+	double deltaK = k1 - k2;
+	double deltaB = b1 - b2;
+	double x = (-1) * deltaB / deltaK;
+	double y = k2 * x + b2;
 
-
+	Console.WriteLine($"Координаты пересечения данных прямых: x = {x}, y = {y}");
+	Console.WriteLine("Для продолжения нажмите 'Ввод'...");
+	Console.ReadLine();
 }
 
 TaskMenu();
